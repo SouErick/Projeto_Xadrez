7f8b320c69a280e300221d0168ca210a3f7b4dff
SouErick <ericksousasaraiva@gmail.com>
2024-03-04 16:50:42 -0300

    Adicionando Class Peça / Class Tabuleiro

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,13 +1,14 @@
 package aplication;
 
 import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Posicao posicao = new Posicao(3, 5);
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 		System.out.println(posicao.toString());
-
 	}
 
 }
