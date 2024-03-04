aadfabe3a6d2854c67edfaff1580c2fafb757c5a
SouErick <ericksousasaraiva@gmail.com>
2024-03-04 17:10:29 -0300

    Inteface do tabuleiro implementada

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,13 +2,13 @@ package aplication;
 
 import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.PartidaXadrez;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Posicao posicao = new Posicao(3, 5);
-		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
-		System.out.println(posicao.toString());
+		PartidaXadrez partida = new PartidaXadrez();
+		UI.printTabuleiro(partida.getPecas());
 	}
 
 }
